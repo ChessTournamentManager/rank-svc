863d38e6eca682d75b7f10d7ff3696b1184b7a01
Judah <i460958@fhict.nl>
2022-09-08 00:39:28 +0200

    Added spring boot project

---
+++ src/main/java/com/chesstournamentmanager/roundsvc/RoundSvcApplication.java
@@ -0,0 +1,13 @@
+package com.chesstournamentmanager.roundsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoundSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoundSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chesstournamentmanager/roundsvc/RoundSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chesstournamentmanager.roundsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoundSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
